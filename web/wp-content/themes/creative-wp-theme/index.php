0043f937b958d692bc10fc9be2992742208f462e
Chris Keller <ckeller.tomgeekery@gmail.com>
2019-08-09 13:54:07 -0400

    Added acf + yoast; breadcrumbs and heading

--- web/wp-content/themes/creative-wp-theme/index.php
+++ web/wp-content/themes/creative-wp-theme/index.php
@@ -9,16 +9,23 @@ while ( have_posts() ) {
 			<?php echo get_the_post_thumbnail( $post->ID, 'full', array( 'class' => 'w-full' ) ); ?>
             <div class="absolute w-full bottom-2">
                 <div class="contained">
-                    <h1 class="bg-michigan-maize text-3xl font-bold inline py-2 px-4 leading-none"><?php the_title(); ?></h1>
+                    <div class="bg-michigan-maize text-3xl font-bold inline py-2 px-4 leading-none uppercase"><?php the_title(); ?></div>
                 </div>
             </div>
         </div>
 	<?php endif; ?>
 
+    <div class="contained mt-2 mb-8">
+		<?php if ( function_exists( 'yoast_breadcrumb' ) ): ?>
+			<?php yoast_breadcrumb( '<p class="text-md font-medium" id="breadcrumbs">', '</p>' ); ?>
+		<?php endif; ?>
+    </div>
+
     <main role="main" id="main" class="contained">
 		<?php get_sidebar(); ?>
 
         <article>
+            <h1 class="text-3xl"><?php the_field('heading'); ?></h1>
 			<?php the_content(); ?>
         </article>
     </main>
