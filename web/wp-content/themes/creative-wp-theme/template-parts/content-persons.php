936e2b19ef616b572fd04b47abb8c3d95d81d670
Chris Keller <ckeller.tomgeekery@gmail.com>
2019-08-23 11:47:03 -0400

    Added content persons file

--- web/wp-content/themes/creative-wp-theme/template-parts/content-persons.php
+++ web/wp-content/themes/creative-wp-theme/template-parts/content-persons.php
@@ -3,43 +3,47 @@
    $args = get_query_var('args');
 
    $loop = new WP_Query( $args );
+   // var_dump ($loop);
    if( $loop->have_posts() ):
       while( $loop->have_posts() ): $loop->the_post(); global $post; ?>
          <li class="mb-16">
             <div class="person flex flex-wrap md:flex-wrap">
-               <div class="person-photo w-full md:w-1/4 md:pr-8">
-               <?php if( get_post_meta($post->ID, 'photo._src.medium', true) ) { ?>
-                  <img class="object-cover w-full" src="<?php echo get_post_meta($post->ID, 'photo._src.medium', true); ?>" alt="<?php echo get_the_title(); ?>" >
+
+            <div class="person-photo w-full md:w-1/4 md:pr-8">
+               <?php if( get_field('photo') ) { ?>
+                  <img class="object-cover w-full" src="<?php the_field('photo'); ?>" alt="<?php the_title(); ?>" >
                <?php } else { ?>
                   <img class="person-photo" src="http://fpoimg.com/250x270?text=FPO—Image to come&bg_color=ec008c&text_color=ffffff" alt="" >
                <?php } ?>
                </div>
                <div class="person-text w-full md:w-3/4">
-               <?php if( get_post_meta($post->ID, 'micda_title', true) ) { ?>
-                  <h4 class="font-xl font-bold text-umblue"><?php echo get_the_title(); ?>, <?php echo get_post_meta($post->ID, 'micda_title', true); ?></h4>
+
+               <?php if( get_field('micda_title') ) { ?>
+                  <h4 class="font-xl font-bold text-umblue"><?php the_title(); ?>, <?php the_field('micda_title'); ?></h4>
                <?php } else { ?>
-                   <h4 class="font-xl font-bold text-umblue"><?php echo get_the_title(); ?></h4>
+                   <h4 class="font-xl font-bold text-umblue"><?php the_title(); ?></h4>
                <?php } ?>
-                  <h5 class="font-normal"><?php echo get_post_meta($post->ID, 'positions', true); ?></h5>
-                  <p class="mt-0 font-light"><?php echo get_post_meta($post->ID, 'education', true); ?></p>
-                  <p class="mt-2"><a href="mailto:<?php echo get_post_meta($post->ID, 'email', true); ?>"><?php echo get_post_meta($post->ID, 'email', true); ?></a></p>
+
+                  <h5 class="font-normal"><?php the_field('positions'); ?></h5>
+                  <p class="mt-0 font-light"><?php the_field('education'); ?></p>
+                  <p class="mt-2"><a href="mailto:<?php the_field('email'); ?>"><?php the_field('email'); ?></a></p>
                   <ul class="person-links mt-2">
-                  <?php if( get_post_meta($post->ID, 'web_page', true) ) { ?>
-                     <li><a href="<?php echo get_post_meta($post->ID, 'web_page', true); ?>">Web Page</a></li>
-                  <?php } ?>
-                  <?php if( get_post_meta($post->ID, 'google_scholar', true) ) { ?>
-                     <li><a href="<?php echo get_post_meta($post->ID, 'google_scholar', true); ?>">Google Scholar</a></li>
-                  <?php } ?>
-                  <?php if( get_post_meta($post->ID, 'pubmed', true) ) { ?>
-                     <li><a href="<?php echo get_post_meta($post->ID, 'pubmed', true); ?>">Pubmed</a></li>
-                  <?php } ?>
+                     <?php if( get_field( 'web_page') ) { ?>
+                        <li><a href="<?php the_field('web_page'); ?>">Web Page</a></li>
+                     <?php } ?>
+                     <?php if( get_field( 'google_scholar' ) ) { ?>
+                        <li><a href="<?php the_field( 'google_scholar' ); ?>">Google Scholar</a></li>
+                     <?php } ?>
+                     <?php if( get_field( 'pubmed' ) ) { ?>
+                        <li><a href="<?php the_field( 'pubmed' ); ?>">Pubmed</a></li>
+                     <?php } ?>
                   </ul>
-                  <span class="hidden last-name"><?php echo get_post_meta($post->ID, 'last_name', true); ?></span>
+                  <span class="hidden last-name"><?php the_field( 'last_name' ); ?></span>
                   <span class="hidden category"><?php
                      $all_terms = wp_get_post_terms($post->ID, 'category');
                      foreach ( $all_terms as $term ) {
-                        echo $term->name;} ?></span>
-                  <?php if( get_post_meta($post->ID, 'interests', true) ) { ?>
+                     echo $term->name;} ?></span>
+                  <?php if( get_the_content() ) { ?>
                   <details class="mt-6">
                      <summary>
                         <span class="accordion-heading">Interests</span>
@@ -48,7 +52,7 @@
                         </span>
                      </summary>
                      <div class="accordion-content">
-                        <?php echo get_post_meta($post->ID, 'interests', true); ?>
+                        <?php the_content(); ?>
                      </div>
                   </details>
                   <?php } ?>
