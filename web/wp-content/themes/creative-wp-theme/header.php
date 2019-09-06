a3affee7ca21e68408d18f07699298db34429a28
Chris Keller <ckeller.tomgeekery@gmail.com>
2019-09-06 14:19:00 -0400

    Added GA

--- web/wp-content/themes/creative-wp-theme/header.php
+++ web/wp-content/themes/creative-wp-theme/header.php
@@ -2,6 +2,16 @@
 <html <?php language_attributes(); ?>>
 
 <head>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-7437009-4"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-7437009-4');
+    </script>
+
     <meta charset="<?php bloginfo( 'charset' ); ?>"/>
     <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
     <meta name="viewport" content="width=device-width, initial-scale=1">
